bf689603d213ec20d63ce6dc2b76554aeae04ba4
arvsing3 <arvsing3@Arvsing3-ws.partnet.cisco.com>
2016-01-11 11:45:13 +0530

    Second commit

--- Demo/src/Com/demo/demo.java
+++ Demo/src/Com/demo/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double x=8.03835616260766983;
+		double y=1234.123444
 		int y=2;
 		//float z=123.45;
 		System.out.println(x);
