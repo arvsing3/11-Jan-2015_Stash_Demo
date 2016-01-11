9255e96ae548858f3a9194573c7b0570f39acde2
arvsing3 <arvsing3@Arvsing3-ws.partnet.cisco.com>
2016-01-11 11:23:24 +0530

    initial commit

---
+++ Demo/src/Com/demo/demo.java
@@ -0,0 +1,14 @@
+package Com.demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x=8.03835616260766983;
+		int y=2;
+		//float z=123.45;
+		System.out.println(x);
+
+	}
+
+}
